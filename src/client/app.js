3ed1778eab00a19b505793917f4ff0b23c3cb72b
Anthony <joansanres@gmail.com>
2019-03-03 17:01:58 -0500

    Added webpack configuration and general blank structure

---
+++ src/client/app.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+
+const jsx = (
+  <h1>This is my page!</h1>
+);
+
+ReactDOM.render(jsx, document.querySelector("#root"));
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+module.exports = (env) => {
+  const isProduction = env === "production";
+  const CSSExtract = new MiniCssExtractPlugin({
+    filename: "styles.css"
+  });
+  
+  return {
+    entry: ["@babel/polyfill", "./src/client/app.js"],
+    output: {
+      path: path.join(__dirname, "public", "dist"),
+      filename: "bundle.js"
+    },
+    module: {
+      rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        }
+      },{
+        test: /\.s?css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              sourceMap: true
+            }
+          }, {
+            loader: "css-loader",
+            options: {
+              sourceMap: true
+            }
+          }, {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }]
+    },
+    plugins: [ CSSExtract ],
+    devtool: isProduction ? "source-map" : "inline-source-map",
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      historyApiFallback: true,
+      publicPath: "/dist/"
+    }
+  } 
+}
